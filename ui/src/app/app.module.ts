48f5e6b4126d64c0ce01777f8d9c12f53601784c
jasminejeane <jasminejeane@gmail.com>
2018-05-30 15:45:17 -0700

    added angular material

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { UserService } from './shared/user/user.service';
 import { HttpClientModule } from '@angular/common/http';
 import { UserListComponent } from './user-list/user-list.component';
 
+import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +17,13 @@ import { UserListComponent } from './user-list/user-list.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
