a53976fde48bbbe823098fcbd8ea2789aa34955b
jasminejeane <jasminejeane@gmail.com>
2018-05-30 14:55:19 -0700

    user service added

---
+++ ui/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { UserService } from './shared/user/user.service';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+
+
+
+export class AppModule { }
