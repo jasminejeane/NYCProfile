4cc8fd1666f90ce48685e7a54c040ce125b96f56
jasminejeane <jasminejeane@gmail.com>
2018-05-30 15:28:54 -0700

    BE  and FE  connencted

--- server/src/main/java/com/example/demo/TopUserController.java
+++ server/src/main/java/com/example/demo/TopUserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 class TopUserController {
