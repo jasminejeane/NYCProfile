8311584ae3914c11565e1d2a12770ec8e0b0a99e
jasminejeane <jasminejeane@gmail.com>
2018-05-30 15:24:27 -0700

    enabled cors

--- server/src/main/java/com/example/demo/TopUserController.java
+++ server/src/main/java/com/example/demo/TopUserController.java
@@ -1,27 +1,33 @@
+package com.example.demo;
+
+//package com.okta.developer.demo;
+
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
 @RestController
-class CoolCarController {
-    private CarRepository repository;
+class TopUserController {
+    private UserRepository repository;
 
-    public CoolCarController(CarRepository repository) {
+    public TopUserController(UserRepository repository) {
         this.repository = repository;
     }
 
-    @GetMapping("/cool-cars")
-    public Collection<Car> coolCars() {
+    @GetMapping("/top-users")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public Collection<User> topUsers() {
         return repository.findAll().stream()
-                .filter(this::isCool)
+                .filter(this::isTop)
                 .collect(Collectors.toList());
     }
 
-    private boolean isCool(Car car) {
-        return !car.getName().equals("AMC Gremlin") &&
-                !car.getName().equals("Triumph Stag") &&
-                !car.getName().equals("Ford Pinto") &&
-                !car.getName().equals("Yugo GV");
+    private boolean isTop(User user) {
+        return !user.getName().equals("AMC Gremlin") &&
+                !user.getName().equals("Triumph Stag") &&
+                !user.getName().equals("Ford Pinto") &&
+                !user.getName().equals("Yugo GV");
     }
 }
--- server/src/main/java/com/example/demo/UserRepository.java
+++ server/src/main/java/com/example/demo/UserRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
 interface UserRepository extends JpaRepository<User, Long> {
 }
