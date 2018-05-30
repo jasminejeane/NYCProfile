af0fa03e2faf9a52a94749f263b203267ed0b597
jasminejeane <jasminejeane@gmail.com>
2018-05-30 15:21:12 -0700

    fixed errors

--- ui/src/app/user-list/user-list.component.ts
+++ ui/src/app/user-list/user-list.component.ts
@@ -1,9 +1,18 @@
+import { Component, OnInit } from '@angular/core';
 import { UserService } from '../shared/user/user.service';
 
-export class UserListComponent implements OnInit {
-users: Array<any>;
 
-constructor(private userService: UserService) { }
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+
+
+export class UserListComponent implements OnInit {
+  users: Array<any>;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
     this.userService.getAll().subscribe(data => {
